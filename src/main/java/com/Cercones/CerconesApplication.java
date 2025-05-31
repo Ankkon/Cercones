60ecf057aaca6815e9615a161ef7740a49fdc367
GuillermoAndresDelgadoCercone <113642438+Ankkon@users.noreply.github.com>
2025-05-31 16:12:10 -0600

    EA-08-Practicas-2

---
+++ src/main/java/com/Cercones/CerconesApplication.java
@@ -0,0 +1,13 @@
+package com.Cercones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerconesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CerconesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Cercones/CerconesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cercones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerconesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
